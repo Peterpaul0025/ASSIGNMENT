075fc6c6bdaa2ec50af2cca97699e36f96b5a771
Zishan Ansari <108237273+Peterpaul0025@users.noreply.github.com>
2025-10-25 12:19:33 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,11 @@
+# Program to create a personalized greeting
+
+# Taking input from the user
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Concatenating first and last name
+full_name = first_name + " " + last_name
+
+# Displaying personalized greeting
+print("Hello,", full_name + " !Welcome to Python programming.")
